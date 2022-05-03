b06d9b39e032041c75418e70790b96b12e9d950a
ayu6910 <ayuny6910@gmail.com>
2022-05-03 19:45:41 -0400

    I did some change

---
+++ Documents/eclipse-workspace/Java_Project_1/src/Demo_Package/Demo_class.java
@@ -0,0 +1,13 @@
+package Demo_Package;
+
+public class Demo_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("print some thing");
+System.out.print("print some thing");
+System.out.print("print some thing");
+System.out.print("print some thing");
+	}
+
+}
